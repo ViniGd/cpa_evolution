9c0150d839374f8a21054f0c9c82dcbe847e21e7
Vinicius-GD <viniciusgd3@gmail.com>
2021-10-26 11:36:36 -0300

    criação widget bandeira

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:cpa_evolution/widgets/bandeira.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -16,7 +17,7 @@ class MyApp extends StatelessWidget {
 
         primarySwatch: Colors.blue,
       ),
-      home: ,
+      home: Bandeira("",""),
     );
   }
 }
---
+++ lib/widgets/bandeira.dart
@@ -0,0 +1,51 @@
+import "package:flutter/material.dart";
+
+class Bandeira extends StatelessWidget {
+
+  String titulo;
+  String descricao;
+
+  Bandeira(this.titulo, this.descricao);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Container(
+          width: MediaQuery
+              .of(context)
+              .size
+              .width * 0.6,
+          height: 500,
+          padding: EdgeInsets.fromLTRB(60, 30, 60, 30),
+          decoration: BoxDecoration(color: Color(0xff004684), borderRadius: BorderRadius.all(Radius.circular(50),),),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+              Text(
+                titulo,
+                style: TextStyle(color: Colors.white, fontSize: 60,),),
+              Text(
+                descricao,
+                style: TextStyle(color: Colors.white, fontSize: 20,),
+              ),
+              GestureDetector(
+                onTap: (){print("bunana");},
+                child: Container(
+                  padding: EdgeInsets.fromLTRB(60, 10, 60, 10),
+                  height: 60,
+                  decoration: BoxDecoration(
+                    color: Color(0xff4DA735),
+                    borderRadius: BorderRadius.all(
+                      Radius.circular(10),
+                    ),
+                  ),
+                  child: Text("Acessar", style: TextStyle(color: Colors.white, fontSize: 30,),),),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
